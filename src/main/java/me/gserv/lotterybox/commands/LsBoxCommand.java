0648a8d1e17252f26b348fbea1b404ab3c37fff4
Gareth Coles <gdude2002@gmail.com>
2014-09-12 11:27:34 +0100

    /lsbox messages done

--- src/main/java/me/gserv/lotterybox/commands/LsBoxCommand.java
+++ src/main/java/me/gserv/lotterybox/commands/LsBoxCommand.java
@@ -22,6 +22,7 @@ public class LsBoxCommand implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         // /lsbox [name] ["rewards"]
 
+        HashMap <String, String> args = new HashMap<>();
         boolean hasPermission = false;
 
         if (commandSender instanceof ConsoleCommandSender ||
@@ -46,103 +47,121 @@ public class LsBoxCommand implements CommandExecutor {
             HashSet<Box> boxes = this.plugin.getDataHandler().getBoxes();
 
             if (boxes.size() < 1) {
-                commandSender.sendMessage("No boxes found");
+                this.plugin.sendMessage(commandSender, "other.no_boxes");
                 return true;
             }
 
             for (Box box : boxes) {
-                commandSender.sendMessage(
-                        String.format(
-                                "%s [%s - %s, %s, %s] - chance: %s",
-                                box.name, box.world, box.x,
-                                box.y, box.z, box.getChance()
-                                )
-                );
+                args.clear();
+
+                args.put("name", box.name);
+                args.put("world", box.world);
+                args.put("x", String.valueOf(box.x));
+                args.put("y", String.valueOf(box.y));
+                args.put("z", String.valueOf(box.z));
+                args.put("chance", String.valueOf(box.getChance()));
+
+                this.plugin.sendMessage(commandSender, "lsbox.box_entry", args);
+
             }
         } else if (strings.length < 2) {
             Box box = this.plugin.getDataHandler().getBox(strings[0]);
 
             if (box == null) {
-                commandSender.sendMessage(String.format("No such box: %s", strings[0]));
+                args.clear();
+                args.put("box", strings[0]);
+
+                this.plugin.sendMessage(commandSender, "other.no_box", args);
                 return true;
             }
 
-            commandSender.sendMessage(String.format("== %s ==", strings[0]));
-            commandSender.sendMessage(String.format("Location: %s - %s, %s, %s", box.world, box.x, box.y, box.z));
-            commandSender.sendMessage(String.format("Chance: %s", box.getChance()));
+            args.clear();
+            args.put("box", box.name);
+            this.plugin.sendMessage(commandSender, "lsbox.box_header", args);
+
+            args.clear();
+            args.put("world", box.world);
+            args.put("x", String.valueOf(box.x));
+            args.put("y", String.valueOf(box.y));
+            args.put("z", String.valueOf(box.z));
+            this.plugin.sendMessage(commandSender, "lsbox.box_location", args);
+
+            args.clear();
+            args.put("chance", String.valueOf(box.getChance()));
+            this.plugin.sendMessage(commandSender, "lsbox.box_chance", args);
+
+            args.clear();
 
             if (box.isInfinite()) {
-                commandSender.sendMessage("Uses: Infinite");
+                args.put("uses", "infinite");
             } else {
-                commandSender.sendMessage(String.format("Uses: %s", box.getUses()));
+                args.put("uses", String.valueOf(box.getUses()));
             }
 
-            commandSender.sendMessage(String.format("Rewards: %s", box.numRewards()));
+            this.plugin.sendMessage(commandSender, "lsbox.box_uses", args);
+
+            args.clear();
+            args.put("count", String.valueOf(box.numRewards()));
+            this.plugin.sendMessage(commandSender, "lsbox.box_rewards_count", args);
 
             if (box.isNamedKeys()) {
-                commandSender.sendMessage("== Requires named keys ==");
+                this.plugin.sendMessage(commandSender, "lsbox.box_named_keys");
             } else {
-                commandSender.sendMessage("== Can use any key ==");
+                this.plugin.sendMessage(commandSender, "lsbox.box_any_keys");
             }
         } else if ("rewards".equalsIgnoreCase(strings[1])) {
             Box box = this.plugin.getDataHandler().getBox(strings[0]);
 
             if (box == null) {
-                commandSender.sendMessage(String.format("No such box: %s", strings[0]));
+                args.clear();
+                args.put("box", strings[0]);
+
+                this.plugin.sendMessage(commandSender, "other.no_box", args);
                 return true;
             }
 
-            commandSender.sendMessage(String.format("== Rewards: %s ==", strings[0]));
+            args.clear();
+            args.put("box", strings[0]);
+
+            this.plugin.sendMessage(commandSender, "lsbox.rewards_header", args);
 
             HashMap<String, HashMap<String, Object>> rewards = box.getRewards();
 
             if (rewards.size() < 1) {
-                commandSender.sendMessage("No rewards found");
+                this.plugin.sendMessage(commandSender, "lsbox.no_rewards");
             }
 
             for (String key : rewards.keySet()) {
                 HashMap<String, Object> reward = rewards.get(key);
+                args.clear();
+                args.put("reward", key);
+                args.put("chance", String.valueOf(reward.get("chance")));
 
                 if ("item".equals(reward.get("type"))) {
                     ItemStack item = ItemStack.deserialize((java.util.Map<String, Object>) reward.get("item"));
-                    commandSender.sendMessage(
-                            String.format(
-                                    "[%s] %s (%s chance) - %s*%s",
-                                    "Item", key, reward.get("chance"),
-                                    item.getType().toString(), item.getAmount()
-                            )
-                    );
-                } else if ("command".equals(reward.get("type"))) {
-                    commandSender.sendMessage(
-                            String.format(
-                                    "[%s] %s (%s chance) - %s",
-                                    "Command", key, reward.get("chance"),
-                                    reward.get("command")
-                            )
-                    );
-                } else if ("money".equals(reward.get("type"))) {
-                    if ((int) reward.get("amount") >= 0) {
-                        commandSender.sendMessage(
-                                String.format(
-                                        "[%s] %s (%s chance) - +%s",
-                                        "Money", key, reward.get("chance"),
-                                        reward.get("amount")
-                                )
-                        );
-                    } else {
-                        commandSender.sendMessage(
-                                String.format(
-                                        "[%s] %s (%s chance) - -%s",
-                                        "Money", key, reward.get("chance"),
-                                        reward.get("amount")
-                                )
-                        );
 
+                    args.put("amount", String.valueOf(item.getAmount()));
+                    args.put("type", item.getType().toString());
+
+                    this.plugin.sendMessage(commandSender, "lsbox.item_reward", args);
+                } else if ("command".equals(reward.get("type"))) {
+                    args.put("command", (String) reward.get("command"));
+
+                    this.plugin.sendMessage(commandSender, "lsbox.command_reward", args);
+                } else if ("money".equals(reward.get("type"))) {
+                    args.put("amount", String.valueOf(reward.get("amount")));
+
+                    if ((int) reward.get("amount") > 0) {
+                        args.put("string", "+");
+                    } else if ((int) reward.get("amount") < 0) {
+                        args.put("string", "-");
                     }
+
+                    this.plugin.sendMessage(commandSender, "lsbox.money_reward", args);
                 }
             }
         } else {
-            commandSender.sendMessage("Usage: /lsbox [name] [\"rewards\"]");
+            this.plugin.sendColouredMessage(commandSender, "&6Usage: &a/&clsbox &d[name] [\"rewards\"]");
         }
 
         return true;
